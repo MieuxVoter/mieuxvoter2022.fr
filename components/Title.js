b4cbd5e136f35932bb93465fe104b1e048cde392
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-04-01 19:58:58 +0200

    first commit

---
+++ components/Title.js
@@ -0,0 +1,4 @@
+const Title = () => <h1 className="ui  header">
+  <span className="ui voter">voter</span><span className="ui utile">utile</span><span className="ui dotfr">.fr</span>
+</h1>
+export default Title
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
+import '../semantic/semantic.min.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
 }
 
