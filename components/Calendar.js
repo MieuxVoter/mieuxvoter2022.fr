35461705d9e5a220013596003754dd41b5931a33
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-04-02 11:38:59 +0200

    feat: menu homepage

---
+++ components/Calendar.js
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+import calendar from '../public/calendar.svg'
+
+const Calendar = () => <div>
+  <Image src={calendar} alt='calendar logo' />
+</div>
+
+export default Calendar
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,48 @@
+import Image from 'next/image'
+import facebook from '../public/facebook.svg'
+import twitter from '../public/twitter.svg'
 import Title from '../components/Title'
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
 
-      <div className="home">
-        <div className="ui inverted vertical masthead center aligned segment">
+      <div className="ui vertical masthead center aligned segment ">
 
-          <div className="ui container">
+        <div className="ui container">
 
-            <div className="ui top secondary menu">
-              <div className="ui container">
-                <div className="right menu">
-                  <a href="https://www.facebook.com/mieuxvoter.fr">
-                    <i className="facebook icon"></i>
-                  </a>
-                  <a href="twitter.com/mieux_voter">
-                    <i className="twitter icon"></i>
-                  </a>
-                  <a className="ui button">En savoir plus</a>
+          <div className="ui top secondary menu">
+            <div className="ui container">
+              <div className="right menu">
+                <a href="https://www.facebook.com/mieuxvoter.fr" className='social'>
+                  <Image src={facebook} alt='facebook logo' />
+                </a>
+                <a href="twitter.com/mieux_voter" className='social'>
+                  <Image src={twitter} alt='twitter logo' />
+                </a>
+                <div className='ui button secondary'>
+                  <a className="ui ensavoirplus" href='/faq'>Questions fréquentes</a>
                 </div>
               </div>
             </div>
-
-          </div>
-
-          <div className="ui text container">
-            <Title />
-            <h2 className='ui header'>Votez utile pour la dernière fois !</h2>
-            <div className="ui huge primary button">Get Started <i className="right arrow icon"></i></div>
           </div>
 
         </div>
 
-
-
-
-
+        <div className="ui text container title">
+          <Title />
+          <h2 className='ui header subtitle'>Votez utile pour la dernière fois !</h2>
+          <p>La plupart des partis recherchent à rallier les électeurs derrière leur candidat avant d'avoir un programme. Nous faisons la démarche inverse. Nous commençons par le programme et vous présenterons le ou la candidate la plus à même de le porter dans quelques semaines.
+          </p>
+          <div className="ui huge primary button">Participer au vote <i className="right arrow icon"></i></div>
+        </div>
 
       </div>
-    </div >
+
+
+
+
+
+    </div>
   )
 }
