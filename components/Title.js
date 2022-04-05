3808bb33862c63ebfcbfc6b9bf6c64992c4a50fd
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-04-05 04:48:23 +0200

    fix: change title

--- components/Title.js
+++ components/Title.js
@@ -1,4 +1,4 @@
 const Title = () => <h1 className="ui  header">
-  <span className="ui voter">voter</span><span className="ui utile">utile</span><span className="ui dotfr">.fr</span>
+  <span className="ui voter">voter</span><span className="ui utile">pourlademocratie</span><span className="ui dotfr">.fr</span>
 </h1>
 export default Title
