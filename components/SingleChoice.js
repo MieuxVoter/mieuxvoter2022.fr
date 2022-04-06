fa58dd4f7c36b04c97957828603bcd030826b4b3
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-04-06 22:35:44 +0200

    fix: texte single choice

--- components/SingleChoice.js
+++ components/SingleChoice.js
@@ -19,14 +19,13 @@ const Candidate = ({name, photo, parti, onSubmit}) => (<div className='ui  candi
 
 
 const SingleChoice = ({onSubmit}) => (<div className='ui container sm'>
-  <h2> Pour présider la France, ayant pris tous les éléments en compte, jugez-vous en conscience
-    que ce(tte) candidat(e) serait…</h2>
+  <h2>Au premier tour de l&apos;élection présidentielle, pour lequel des candidats suivants avez-vous le plus chance de voter ?</h2>
   <p>
     Le scrutin uninominal majoritaire est le mode de scrutin en vigueur pour l&apos;élection
     présidentielle de 2022. Il demande à l’électeur de choisir un candidat.
   </p>
   <p>
-    Dans le cadre de cette étude, vous êtes amenés à vous prononcer sur les candidats à l&apos;élection présidentielle selon deux modes de scrutin différents : le scrutin
+    Dans le cadre de cette étude, vous êtes amenés à vous prononcer anonynement sur les candidats à l&apos;élection présidentielle selon deux modes de scrutin différents : le scrutin
     uninominal majoritaire et le scrutin par jugement majoritaire.
   </p>
   <div className='ui doubling stackable grid three column'>
