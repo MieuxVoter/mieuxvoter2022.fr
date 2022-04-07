8d776a2c2de2e001cce0074cd389035726eb1a19
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-04-07 15:00:16 +0200

    fix: headers

--- components/Title.js
+++ components/Title.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 
-const Title = () => <Link href='/'><h1 className="ui  logo header pointer">
+const Title = () => <Link href='/'><h1 className="ui header logo pointer">
   <span className="ui voter">mieuxvoter</span><span className="ui utile">2022</span><span className="ui dotfr">.fr</span>
 </h1></Link>
 export default Title
